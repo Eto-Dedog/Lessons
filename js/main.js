35f3365f47025946fb901fd8d68a8a1b5155939b
mariapoduzova <mariapoduzova60@gmail.com>
2021-12-01 18:10:46 +0300

    калькулятор в модальном окне

--- js/main.js
+++ js/main.js
@@ -1,10 +1,28 @@
+// объявление переменных
 const btnCart = document.getElementById('cart')
 const modal = document.querySelector('.modal')
 const btnClose = document.querySelector('.btn-close')
+const rows = modal.querySelectorAll('.row');
+const total = modal.querySelector('.modal-sum');
 
+// объявление функций
 const modalOpen = () => {
     modal.classList.toggle('modal--close')
 }
+
+const getFullPrice = () => {
+    let fullPrice = 0;
+    rows.forEach(row => {
+        let priceBlock = row.querySelector('.price')
+        let price = +priceBlock.textContent
+        fullPrice += price;
+    })
+    total.textContent = fullPrice
+}
+
+// вызов функций
+getFullPrice()
+
 
 btnCart.addEventListener('click' , modalOpen);
 btnClose.addEventListener('click' , modalOpen);
@@ -14,4 +32,45 @@ modal.addEventListener('click', (event) => {
         modalOpen()
     }
 })
+
+rows.forEach(row => {
+    let newPrice = 0;
+    let priceBlock = row.querySelector('.price')
+    let price = +priceBlock.textContent
+    let countBlock = row.querySelector('.count')
+    let count = countBlock.textContent
+    const btnMinus = row.querySelector('.minus')
+    const btnPlus = row.querySelector('.plus')
+
+
+    const getNewPrice = (count, price) => {
+        newPrice = count * price
+        priceBlock.textContent = newPrice
+        getFullPrice()
+    }
+
+    btnMinus.addEventListener('click', () => {
+        if (count > 0) {
+            count--
+            countBlock.textContent = count
+            getNewPrice(count, price)
+        }
+    })
+    btnPlus.addEventListener('click', () => {
+        count++
+        countBlock.textContent = count
+        getNewPrice(count, price)
+    })
+})  
+
+// логеры
+
+
+
+
+
+
+
+
+
 
